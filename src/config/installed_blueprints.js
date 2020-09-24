f7ae45ab5e2c12a0ac941fa0446d9a0633daea1d
Crowdbotics <team@crowdbotics.com>
2020-09-24 07:14:13 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings108524', human_name: 'Settings', access_route: 'Settings108524'},
 { name: 'UserProfile108522', human_name: 'User Profile', access_route: 'UserProfile108522'},
 { name: 'BlankScreen0108500', human_name: 'Blank Screen 0', access_route: 'BlankScreen0108500'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings108524Navigator from '../features/Settings108524/navigator';
 import UserProfile108522Navigator from '../features/UserProfile108522/navigator';
 import BlankScreen0108500Navigator from '../features/BlankScreen0108500/navigator';
 
@@ -15,6 +16,7 @@ import BlankScreen0108500Navigator from '../features/BlankScreen0108500/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Settings108524: { screen: Settings108524Navigator },
 UserProfile108522: { screen: UserProfile108522Navigator },
 BlankScreen0108500: { screen: BlankScreen0108500Navigator },
 
